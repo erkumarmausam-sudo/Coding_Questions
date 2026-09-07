626877790cd23a8dd0f16caadcca2d2dc8f70bcc
Mausam Kumar <er.kumar.mausam@gmail.com>
2026-09-07 22:02:03 +0530

    Time: 0 ms (100%), Space: 7.8 MB (73.07%) - LeetHub

---
+++ 2520-count-the-digits-that-divide-a-number/2520-count-the-digits-that-divide-a-number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int countDigits(int num) {
+     int  val = num;
+       int count = 0;
+     while( val !=0){
+        int rem = val % 10;
+        if(num % rem == 0)
+        count++;
+        val = val/10;
+     }
+     return count;
+
+    }
+};
